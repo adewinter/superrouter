7fa47093c44d197aaa6d8be466bc6eecd87189c3
Anton de Winter <adewinter@gmail.com>
2012-07-01 09:48:24 -0400

    Finally starting on the actual content. Sane defaults for static files

--- srweb/settings.py
+++ srweb/settings.py
@@ -74,3 +74,33 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'srweb.frontend',
 )
+
+
+import os
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(__file__, '..', '..', '..', 'media_files')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(__file__,'..', '..', '..', 'static_files')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+CWD = os.path.join(os.path.curdir, 'srweb')
+BASE_STATIC = os.path.join(CWD, 'static')
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    BASE_STATIC,
+    )
